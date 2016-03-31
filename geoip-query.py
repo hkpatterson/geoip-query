79b8327d16bad06fc0618b01a52c80998c6f5f25
Heath Patterson <hpatterson@gmail.com>
2016-03-31 09:53:29 -0500

    Using local variabes and reusing function

--- geoip-query.py
+++ geoip-query.py
@@ -10,34 +10,29 @@ parser.add_argument('-f', metavar='file.csv', type=str, help='file with IP addre
 parser.add_argument('-i', metavar='x.x.x.x', type=str, help='Insert IP address.')
 args = parser.parse_args()
 
-def ip_lookup_argv():
-	try:
-		match = reader.city(args.i)
-		dnslookup = socket.gethostbyaddr(args.i)
-		print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
-	except socket.herror:
-		print "unkown host" + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
-	except geoip2.errors.AddressNotFoundError:
-		print "%s not in Database" % args.i
- 
-def ip_lookup_csv():
-	with open(args.f) as txt_file:
-		ip_read = txt_file.read()
-		for ipaddr in re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_read):
-			match = reader.city(ipaddr)
-			try:
-				dnslookup = socket.gethostbyaddr(ipaddr)
-				print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
-			except socket.herror:
-				print "unkown host" + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
-			except geoip2.errors.AddressNotFoundError:
-				print "%s not in Database" % args.i
-if args.f:
-	print ip_lookup_csv()
-elif args.i:
-	print ip_lookup_argv()
-else:
-	print """
+def ip_lookup(ip):
+    try:
+        match = reader.city(ip)
+        dnslookup = socket.gethostbyaddr(ip)
+        print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+    except socket.herror:
+        print "unkown host" + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+    except geoip2.errors.AddressNotFoundError:
+        print "%s not in Database" % ip
+
+def ip_lookup_csv(ipfile):
+    with open(ipfile) as txt_file:
+        ip_read = txt_file.read()
+        for ipaddr in re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_read):
+            ip_lookup(ipaddr)
+
+if __name__ == "__main__":
+    if args.f:
+        ip_lookup_csv(args.f)
+    elif args.i:
+        ip_lookup(args.i)
+    else:
+        print """
 optional arguments:
   -h, --help   show this help message and exit
   -f file.csv  file with IP addresses comma seperated.
